00241e92a5c2038ac53aa9f6c31e5f757a473bbf
Ian Duncan <ian@iankduncan.com>
2020-06-14 21:23:16 -0700

    Typescript fix for babel plugin

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,16 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+interface BlogFrontMatter {
+  __resourcePath: string;
+  // And so on...
+  title: string;
+  blurb?: string;
+  tag?: string;
+}
+
+declare module "*.mdx" {
+  let MDXComponent: (props: any) => JSX.Element;
+  export default MDXComponent;
+  export const frontMatter: BlogFrontMatter[];
+}
